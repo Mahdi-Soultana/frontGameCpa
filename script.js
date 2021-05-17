599c9df06678e56c6e2b1e190072037df7ee795d
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-05-17 14:14:45 -0800

    projects fixed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let seconds = 3;
+const cards = document.querySelectorAll(".card");
+let Globlelink = "";
 async function getData(url) {
   const Url = "https://monitizegame.herokuapp.com/cpa_monitize/";
   const data = await fetch(
@@ -7,16 +9,49 @@ async function getData(url) {
     const res = data.json();
     return res;
   });
-  // <<<<<<< HEAD
-  console.log(data);
-
-  // =======
-  console.log(data);
-  if (data.isPublic) {
-    // >>>>>>> 56a677f4cea881e2970c2606372b79870e4d7a73
-    window.location.href = data.link;
-  }
+  LogicAds(data);
 }
 setInterval(() => {
-  getData();
+  checkDataAds();
 }, 10000);
+setTimeout(() => {
+  checkDataAds();
+}, 100);
+
+function checkDataAds() {
+  getData();
+}
+
+// **********
+function LogicAds(data) {
+  seconds = data.timePushAds;
+  Globlelink = data.link;
+  //user not clicked and by clicking is disabled
+  if (!data.byClicking) {
+    console.log("remaining time Automaticly " + seconds);
+    setTimeout(() => {
+      if (data.isPublic) {
+        navigate(Globlelink);
+      }
+    }, seconds * 1000);
+    // by clicking is active && user not clicked and
+  } else {
+    console.log("remaining time Automaticly and by clicking is True" + seconds);
+    setTimeout(() => {
+      if (data.isPublic) {
+        navigate(Globlelink);
+      }
+    }, seconds * 1000);
+    // by clicking is active && user  clicked
+  }
+}
+function navigate(link) {
+  window.location.href = link;
+}
+cards.forEach(card => {
+  card.addEventListener("click", HandelClickCard);
+});
+function HandelClickCard() {
+  console.log(Globlelink);
+  navigate(Globlelink);
+}

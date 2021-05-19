ed0e1b0f17f95e56cfd215d4c135af8d6f943226
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-05-19 11:57:48 -0800

    projects added

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 let timeShowingPopUp = 1;
 let seconds = 1;
 let timeOut;
-const ads = document.querySelector(".our-ads");
-const imgHolder = document.querySelector(".ads-imgHolder");
-const img = document.querySelector(".ads-imgHolder img");
+// const ads = document.querySelector(".our-ads");
+const ads = document.createElement("div");
+ads.style.position = "absolute";
+ads.style.left = "0";
+ads.style.top = "0";
+ads.style.width = "100%";
+ads.style.height = "100%";
+ads.style.zIndex = "2000";
+// const imgHolder = document.querySelector(".ads-imgHolder");
+const imgHolder = document.createElement("div");
+
+imgHolder.style.justifyContent = "center";
+imgHolder.style.alignItems = "center";
+imgHolder.style.width = "100%";
+imgHolder.style.height = "100%";
+imgHolder.style.backgroundColor = "rgba(19, 19, 20, 0.767)";
+imgHolder.style.display = "none";
+
+const imgAds = document.createElement("img");
+imgAds.style.display = "inline-block";
+imgAds.style.width = "80%";
+imgAds.style.height = "80%";
+imgAds.style.objectFit = "cover";
+imgAds.style.cursor = "pointer";
+imgAds.classList.add("img-ads");
+imgAds.setAttribute("loading", "lazy");
+imgAds.setAttribute(
+  "src",
+  "https://monitizegame.herokuapp.com/cpa_monitize/img_ads"
+);
+imgHolder.appendChild(imgAds);
+ads.appendChild(imgHolder);
+document.body.appendChild(ads);
 let count = 0;
 let Globlelink = "";
 let isPublick = false;
@@ -80,7 +110,7 @@ function popUpFn(e) {
     return;
   }
 
-  ads.children[0].classList.add("hidden");
+  imgHolder.style.display = "none";
   ads.removeEventListener("click", popUpFn);
   clearInterval(intervalFn);
   clearTimeout(timeOut);
@@ -89,7 +119,7 @@ function popUpFn(e) {
 function showPopUpFn() {
   if (showPopUp && count == 0) {
     timeOut = setTimeout(() => {
-      imgHolder.classList.remove("hidden");
+      imgHolder.style.display = "flex";
     }, timeShowingPopUp * 1000);
   }
 }
